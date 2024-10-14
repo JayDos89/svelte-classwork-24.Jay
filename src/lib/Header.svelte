42976c15c50553f642856bd5b8cf09017aaa301c
Jayden Everett <everjr1@student.op.ac.nz>
2024-10-15 12:02:30 +1300

    added files

---
+++ src/lib/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let headingTitle;
+</script>
+    
+<header>
+
+    <h1>{headingTitle}</h1>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">about</a>About</li>
+            <li><a href="/">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Header from "$lib/Header.svelte"
+</script>
+
+<Header headingTitile="About us" />
+<p>Lerem</p>
+
+<style>
+
+</style>
